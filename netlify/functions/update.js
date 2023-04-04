0f6d74b3b887692b47847c2aae4a1fd4010b7c0a
Antonio <zolotuhin87@gmail.com>
2023-04-04 22:39:27 +0300

    local dev setup

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,6 +1,7 @@
 const axios = require('axios').default;
 const sendMessage = require('../../sendMessage');
 exports.handler = async (event) => {
+    console.log('You have got a message', event.body);
     const { message } = JSON.parse(event.body);
     await sendMessage(message.chat.id, 'I got your message!');
     return { statusCode: 200 };
