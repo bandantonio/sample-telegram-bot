bef0b6589db128a169981767b2af9b5df856b055
Antonio <zolotuhin87@gmail.com>
2023-05-10 21:36:21 +0300

    test basic scheduled function

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,19 +1,23 @@
-const { schedule } = require("@netlify/functions");
-const checkStackOverflow = require('../../checkStackOverflow');
-const sendMessage = require('../../sendMessage');
+// const { schedule } = require("@netlify/functions");
+// const checkStackOverflow = require('../../checkStackOverflow');
+// const sendMessage = require('../../sendMessage');
 
-const handler = async (event) => {
-    console.log('You have got a message', event.body);
-    const { message } = JSON.parse(event.body);
+exports.handler = async (event, context) => {
+    console.log(event.body)
 
-    if (message.text.match(/\/check/)) {
-        const days = await checkStackOverflow();
-        await sendMessage(message.chat.id, `🔑 ${days}\n📅 Last checked on ${new Date().toLocaleString()}`);
-    } else {
-        await sendMessage(message.chat.id, `I don't know what you mean`);
-    }
+    // console.log('You have got a message', event.body);
+    // const { message } = JSON.parse(event.body);
 
-  return { statusCode: 200 };
+    // if (message.text.match(/\/check/)) {
+    //     const days = await checkStackOverflow();
+    //     await sendMessage(message.chat.id, `🔑 ${days}\n📅 Last checked on ${new Date().toLocaleString()}`);
+    // } else {
+    //     await sendMessage(message.chat.id, `I don't know what you mean`);
+    // }
+
+    return {
+        statusCode: 200
+    };
 };
 
-exports.handler = schedule("*/3 * * * *", handler);
+// exports.handler = schedule("*/3 * * * *", handler);
