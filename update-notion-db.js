fc81399732e76c9a5ac4fe979e129d4b91d7c127
Antonio <zolotuhin87@gmail.com>
2023-05-14 01:22:05 +0300

    project cleanup

--- update-notion-db.js
+++ update-notion-db.js
@@ -1,12 +1,13 @@
 const { Client } = require("@notionhq/client");
 module.exports = async (days, token, databaseId) => {
     const notion = new Client({ auth: token });
+    const trimmedDaysString = days.replace(/^"(.*)"$/, '$1');
 
     await notion.pages.create({
         parent: { database_id: databaseId },
         properties: {
             Status: {
-                title: [{ text: { content: days } }]
+                title: [{ text: { content: trimmedDaysString } }]
             },
         },
     });
