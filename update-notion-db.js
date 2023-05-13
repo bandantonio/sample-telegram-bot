ec08ed42709d3832529c9487e64bba7b8a7c6c3b
Antonio <zolotuhin87@gmail.com>
2023-05-14 00:49:50 +0300

    set proper secret paths

--- update-notion-db.js
+++ update-notion-db.js
@@ -1,8 +1,7 @@
 const { Client } = require("@notionhq/client");
-const notion = new Client({ auth: process.env.NOTION_TOKEN });
-const databaseId = process.env.DB_ID;
+module.exports = async (days, token, databaseId) => {
+    const notion = new Client({ auth: token });
 
-module.exports = async (days) => {
     await notion.pages.create({
         parent: { database_id: databaseId },
         properties: {
@@ -11,5 +10,6 @@ module.exports = async (days) => {
             },
         },
     });
-  return days;
+
+    return days;
 }
